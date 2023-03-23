ef8845ef929c605603ceb52f36606c919232240f
Michal Kasiarz <michal.kasiarz@protonmail.com>
2023-03-23 14:46:27 +0100

    blockchain controller added

---
+++ controller/blockchain_controller.py
@@ -0,0 +1,14 @@
+class BlockchainController:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def add_block(self, data):
+        self.model.add_block(data)
+        self.view.display()
+
+    def validate_chain(self):
+        if self.model.is_valid():
+            print("Chain is valid")
+        else:
+            print("Chain is not valid")
