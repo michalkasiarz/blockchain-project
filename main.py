fa95cf0e5968bb80b0b97530ef07a69d111dcb81
Michal Kasiarz <michal.kasiarz@protonmail.com>
2023-03-25 10:53:43 +0100

    statistics display function added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from model.block import Block
 from model.blockchain import Blockchain
 from model.logger import Logger
+from model.statistics import Statistics
 from view.blockchain_view import BlockchainView
 from controller.blockchain_controller import BlockchainController
 
@@ -9,6 +10,8 @@ if __name__ == '__main__':
     model = Blockchain.get_instance(block_class=Block)
     logger = Logger()
     model.register_observer(logger)
+    statistics = Statistics()
+    model.register_observer(statistics)
     view = BlockchainView(model)
     controller = BlockchainController(model, view)
     controller.add_block("Charlie's block, my dog is nice")
@@ -17,3 +20,5 @@ if __name__ == '__main__':
     controller.add_block("Agnes' block: I like jazz")
     print("\n")
     view.display_blockchain()
+    print("\n")
+    statistics.display_statistics()
---
+++ model/statistics.py
@@ -0,0 +1,26 @@
+# model/statistics.py
+import datetime
+
+class Statistics:
+    def __init__(self):
+        self.previous_block_timestamp = None
+        self.total_time = datetime.timedelta()
+        self.blocks_count = 0
+
+    def update(self, event, data=None):
+        if event == "block_added":
+            self.update_statistics(data)
+
+    def update_statistics(self, block):
+        if self.blocks_count > 0:
+            time_difference = block.timestamp - self.previous_block_timestamp
+            self.total_time += time_difference
+        self.previous_block_timestamp = block.timestamp
+        self.blocks_count += 1
+
+    def display_statistics(self):
+        if self.blocks_count > 1:
+            average_time = self.total_time / (self.blocks_count - 1)
+            print(f"Average time between blocks: {average_time}")
+        else:
+            print("Not enough blocks to calculate average time.")
