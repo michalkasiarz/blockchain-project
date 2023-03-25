69cab017a7cfb1a03ce7aa80ae179e9422bfbe02
Michal Kasiarz <michal.kasiarz@protonmail.com>
2023-03-25 21:06:27 +0100

    accounts model added

---
+++ model/accounts.py
@@ -0,0 +1,12 @@
+class Accounts:
+    def __init__(self):
+        self.accounts = {
+            "Alice": 100,
+            "Bob": 50,
+            "Charlie": 200
+        }
+
+    def get_balance(self, account):
+        if account not in self.accounts:
+            raise ValueError(f"Account {account} does not exist.")
+        return self.accounts[account]
